09509c47b76f9969342c37ff761d72860f937b66
Joao <jo4o.fds@gmail.com>
2023-03-18 12:38:30 -0300

    primeiro commit

---
+++ app/Controllers/Produto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Produto 
+{
+    public function __construct()
+    {
+        echo 'Product Controller';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Controllers\Produto;
+
+require_once __DIR__ . "./../app/autoload.php";
+
+$produto = new Produto();
