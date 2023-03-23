34470a54fd626848a94ccebf42c12d940a4e8223
Joao <jo4o.fds@gmail.com>
2023-03-23 10:30:13 -0300

    removendo vendas e itens do sql

--- sql/mercado.sql
+++ sql/mercado.sql
@@ -62,22 +62,15 @@ CREATE TABLE vendas_itens (
 );
 
 INSERT INTO clientes(nome, cpf, data_cadastro)
-    VALUES('AVULSO', '12345678911', current_timestamp);
+    VALUES('avulso', '12345678911', current_timestamp);
 
 INSERT INTO categorias(nome, percentual, data_cadastro)
-    VALUES('OUTROS', 10, current_timestamp);
+    VALUES
+    ('biscoitos', 0.18, current_timestamp),
+    ('massas', 0.44, current_timestamp);
 
 INSERT INTO produtos(nome, valor, qtde, categoria_id, data_cadastro)
     VALUES
-    ('PRODUTO 1', 2.99, 3, 1, current_timestamp),
-    ('PRODUTO 2', 4.90, 5, 1, current_timestamp);
-
-INSERT INTO vendas(cliente_id, total_venda, total_imposto, data_cadastro)
-    VALUES
-    (1, 8.97, 0.89, current_timestamp),
-    (1, 24.50, 2.45, current_timestamp);
-    
-INSERT INTO vendas_itens(venda_id, produto_id, qtde, preco_unt, total, total_imposto)
-    VALUES
-    (1, 1, 3, 2.99, 8.97, 0.30),
-    (2, 2, 5, 4.90, 24.50, 2.45);
+    ('rosquinha mabel', 11.49, 60, 1, current_timestamp),
+    ('macarrao 500g', 4.90, 70, 2, current_timestamp),
+    ('pao frances 50g', 1.19, 100, 2, current_timestamp);
